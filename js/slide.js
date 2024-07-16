35f9ef0194a3cebab623cf3630980837e5387b95
sdlima4025 <sergiolimafg3@gmail.com>
2024-07-16 16:47:26 -0300

    criado reset css

---
+++ js/slide.js
@@ -0,0 +1,18 @@
+export default class Slide {
+constructor(slide, wrapper) {
+  this.slide = document.querySelector(slide);
+  this.wrapper = document.querySelector(wrapper);
+}
+
+onStart(event) {
+  event.preventDefault();
+}
+addSlideEvents() {
+  this.wrapper.addEventListener('mousedown', this.onStart);
+}
+
+init() {
+  this.addSlideEvents();
+  return this;
+}
+}
