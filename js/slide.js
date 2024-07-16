480a90296517bef0b9f21bd73ccbfe63bc87ea4d
sdlima4025 <sergiolimafg3@gmail.com>
2024-07-16 16:48:56 -0300

    efetuado import e export de slide

--- js/slide.js
+++ js/slide.js
@@ -1,18 +1,18 @@
 export default class Slide {
-constructor(slide, wrapper) {
-  this.slide = document.querySelector(slide);
-  this.wrapper = document.querySelector(wrapper);
-}
+  constructor(slide, wrapper) {
+    this.slide = document.querySelector(slide);
+    this.wrapper = document.querySelector(wrapper);
+  }
 
-onStart(event) {
-  event.preventDefault();
-}
-addSlideEvents() {
-  this.wrapper.addEventListener('mousedown', this.onStart);
-}
+  onStart(event) {
+    event.preventDefault();
+  }
+  addSlideEvents() {
+    this.wrapper.addEventListener("mousedown", this.onStart);
+  }
 
-init() {
-  this.addSlideEvents();
-  return this;
-}
+  init() {
+    this.addSlideEvents();
+    return this;
+  }
 }
